50e0874c232d15cc68373faa4a188a69c1512916
LucasHilarioFavaroLeal <lucas.hilario.favaro@gmail.com>
2016-10-06 15:27:25 -0300

    Implementação Beta de Servidor

---
+++ app/src/main/java/com/example/myfirstapp/JSONParser.java
@@ -0,0 +1,162 @@
+package com.example.myfirstapp;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.Map;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.util.Log;
+
+public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    // function get json from url
+    // by making HTTP POST or GET mehtod
+    public JSONObject makeHttpRequest(String urlField, String method,
+                                      Map<String,Object> params) {
+
+        // Making HTTP request
+        try {
+            URL url = new URL(urlField);
+
+            StringBuilder postData = new StringBuilder();
+            for (Map.Entry<String,Object> param : params.entrySet()) {
+                if (postData.length() != 0) postData.append('&');
+                postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
+                postData.append('=');
+                postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
+            }
+            byte[] postDataBytes = postData.toString().getBytes("UTF-8");
+
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
+            conn.setDoOutput(true);
+            conn.getOutputStream().write(postDataBytes);
+
+            is = conn.getInputStream();
+        } catch (Exception e) {
+            Log.e("Conn Error", e.toString());
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "UTF-8"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}
+
+/*public class JSONParser {
+
+    static InputStream is = null;
+    static JSONObject jObj = null;
+    static String json = "";
+
+    // constructor
+    public JSONParser() {
+
+    }
+
+    // function get json from url
+    // by making HTTP POST or GET mehtod
+    public JSONObject makeHttpRequest(String url, String method,
+                                      List<NameValuePair> params) {
+
+        // Making HTTP request
+        try {
+
+            // check for request method
+            if(method == "POST"){
+                // request method is POST
+                // defaultHttpClient
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(url);
+                httpPost.setEntity(new UrlEncodedFormEntity(params));
+
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+
+            }else if(method == "GET"){
+                // request method is GET
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                String paramString = URLEncodedUtils.format(params, "utf-8");
+                url += "?" + paramString;
+                HttpGet httpGet = new HttpGet(url);
+
+                HttpResponse httpResponse = httpClient.execute(httpGet);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+            }
+
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(
+                    is, "iso-8859-1"), 8);
+            StringBuilder sb = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+            is.close();
+            json = sb.toString();
+        } catch (Exception e) {
+            Log.e("Buffer Error", "Error converting result " + e.toString());
+        }
+
+        // try parse the string to a JSON object
+        try {
+            jObj = new JSONObject(json);
+        } catch (JSONException e) {
+            Log.e("JSON Parser", "Error parsing data " + e.toString());
+        }
+
+        // return JSON String
+        return jObj;
+
+    }
+}*/
--- app/src/main/java/com/example/myfirstapp/MapsActivity.java
+++ app/src/main/java/com/example/myfirstapp/MapsActivity.java
@@ -1,10 +1,19 @@
 package com.example.myfirstapp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.location.Location;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.ListAdapter;
+import android.widget.SimpleAdapter;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
@@ -18,22 +27,53 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+    ArrayList<HashMap<String, String>> userList;
+    private static String url_database = "http://192.168.0.100:54321/android/db_read_all.php";
+
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_USERS = "users";
+    private static final String TAG_PID = "pid";
+    private static final String TAG_NAME = "name";
+    private static final String TAG_POSITIONX = "positionX";
+    private static final String TAG_POSITIONY = "positionY";
+
+    // products JSONArray
+    JSONArray users = null;
+
     private GoogleMap mMap;
-
     private GoogleApiClient mGoogleApiClient;
-
     private Location mLastLocation;
-
     private Circle radarCircle;
-
     private Marker myPosition;
+    private ArrayList<Marker> markerList = new ArrayList<Marker>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        // Hashmap for ListView
+        userList = new ArrayList<HashMap<String,String> >();
+
+        // Loading products in Background Thread
+        new LoadAllUsers().execute();
 
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -43,11 +83,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
 
         if (mGoogleApiClient == null) {
+            // ATTENTION: This "addApi(AppIndex.API)"was auto-generated to implement the App Indexing API.
+            // See https://g.co/AppIndexing/AndroidStudio for more information.
             mGoogleApiClient = new GoogleApiClient.Builder(this)
                     .enableAutoManage(this /* FragmentActivity */,
                             this /* OnConnectionFailedListener */)
                     .addApi(LocationServices.API)
-                    .build();
+                    .addApi(AppIndex.API).build();
         }
 
     }
@@ -131,11 +173,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void updateMap() {
 
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        LatLng test = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        //LatLng test = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        LatLng test = new LatLng(0,0);
         radarCircle.setCenter(test);
         myPosition.setPosition(test);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(test));
-
     }
 
 
@@ -148,4 +190,113 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         startActivity(intent);
     }
 
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class LoadAllUsers extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(MapsActivity.this);
+            pDialog.setMessage("Loading users. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            Map<String,Object> params = new LinkedHashMap<>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_database, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("All Users: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // products found
+                    // Getting Array of Products
+                    users = json.getJSONArray(TAG_USERS);
+
+                    // looping through All Products
+                    for (int i = 0; i < users.length(); i++) {
+                        JSONObject c = users.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString(TAG_PID);
+                        String name = c.getString(TAG_NAME);
+                        String positionX = c.getString(TAG_POSITIONX);
+                        String positionY = c.getString(TAG_POSITIONY);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<>();
+
+                        // adding each child node to HashMap key => value
+                        map.put(TAG_PID, id);
+                        map.put(TAG_NAME, name);
+                        map.put(TAG_POSITIONX, positionX);
+                        map.put(TAG_POSITIONY, positionY);
+
+                        // adding HashList to ArrayList
+                        userList.add(map);
+                        markerList.clear();
+                        for(int j = 0; j < userList.size(); j++) {
+                            markerList.add(mMap.addMarker(new MarkerOptions()
+                                    .position(new LatLng(Double.parseDouble(userList.get(j).get(TAG_POSITIONX)), Double.parseDouble(userList.get(j).get(TAG_POSITIONY))))
+                                    .title(userList.get(j).get(TAG_NAME))));
+                            Log.e("Is this really expected", userList.get(j).get(TAG_POSITIONX));
+                        }
+                    }
+                } else {
+                    // no products found
+                    // Launch Add New product Activity
+                    //Intent i = new Intent(getApplicationContext(),
+                           // NewProductActivity.class);
+                    // Closing all previous activities
+                    //i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    //startActivity(i);
+                }
+            } catch (JSONException e) {
+                Log.e("JSON Exception", e.toString());
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            //runOnUiThread(new Runnable() {
+            //    public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+            /*        ListAdapter adapter = new SimpleAdapter(
+                            MapsActivity.this, productsList,
+                            R.layout.list_item, new String[] { TAG_PID,
+                            TAG_NAME},
+                            new int[] { R.id.pid, R.id.name });
+                    // updating listview
+                    setListAdapter(adapter);
+                }
+            }); */
+
+        }
+
+    }
 }
